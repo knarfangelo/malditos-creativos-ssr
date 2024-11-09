4433e65404467386236c03c71b40fa7a4595ba2a
knarfangelo <knarf2003angelo@gmail.com>
2024-11-09 13:58:49 -0500

    terminando

--- src/app/layouts/homeLayout/planPrecios/planPrecios.component.ts
+++ src/app/layouts/homeLayout/planPrecios/planPrecios.component.ts
@@ -11,13 +11,13 @@ import { IPlanPrecios, planPrecios } from './mantenimiento/planPrecios';
   template: `
   <header>
     <section class="buttons">
-      <button class="d-web" >DISEÑO WEB</button>
-      <button class="d-grafico" >DISEÑO GRÁFICO</button>
-      <button class="p-digital">PAQUETE DIGITAL</button>
+    <button class="d-web" (click)="showFullscreenCard(items[0])">DISEÑO WEB</button>
+<button class="d-grafico" (click)="showFullscreenCard(items[1])">DISEÑO GRÁFICO</button>
+<button class="p-digital" (click)="showFullscreenCard(items[2])">PAQUETE DIGITAL</button>
     </section>
     <main>
       <section *ngFor="let item of items" 
-               class="card" [ngClass]="">
+               class="card" [ngClass]="{'fullscreen': selectedPlan === item}">
         <div class="encabezado">
           <img class="fondo" src="planes/bc-planes-1.png" alt="">
           <img class="sticker" src="planes/sticker.png" alt="">
@@ -33,6 +33,25 @@ import { IPlanPrecios, planPrecios } from './mantenimiento/planPrecios';
         </div>
       </section>
     </main>
+
+    <!-- Fullscreen Overlay for Selected Card -->
+    <div *ngIf="selectedPlan" class="fullscreen-overlay" (click)="closeFullscreenCard()">
+      <section class="card fullscreen-card" (click)="$event.stopPropagation()">
+        <div class="encabezado">
+          <img class="fondo" src="planes/bc-planes-1.png" alt="">
+          <img class="sticker" src="planes/sticker.png" alt="">
+          <h2>{{ selectedPlan?.title }}</h2>
+        </div>
+        <div class="plan-container">
+          <div *ngFor="let plan of selectedPlan?.plans" class="info">
+            <h1>{{ plan.name }}<br>{{ plan.price }}</h1>
+            <ol>
+              <li *ngFor="let feature of plan.features">{{ feature }}</li>
+            </ol>
+          </div>
+        </div>
+      </section>
+    </div>
   </header>
   `,
   styleUrls: ['./planPrecios.component.css'],
@@ -40,7 +59,14 @@ import { IPlanPrecios, planPrecios } from './mantenimiento/planPrecios';
 })
 export class PlanPreciosComponent { 
   items: IPlanPrecios[] = planPrecios;
+  selectedPlan: IPlanPrecios | null = null;
 
+  showFullscreenCard(plan: IPlanPrecios) {
+    this.selectedPlan = plan;
+  }
 
-
+  closeFullscreenCard() {
+    this.selectedPlan = null;
+  }
 }
+
