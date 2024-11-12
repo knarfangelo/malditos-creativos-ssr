eaa1c967f72587a7807dd9aef088fadedd99c907
knarfangelo <knarf2003angelo@gmail.com>
2024-11-12 16:43:38 -0500

    op imagens

--- src/app/layouts/homeLayout/homeNuestroEquipo/homeNuestroEquipo.component.ts
+++ src/app/layouts/homeLayout/homeNuestroEquipo/homeNuestroEquipo.component.ts
@@ -1,24 +1,30 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, Inject, PLATFORM_ID } from '@angular/core';
+import { register } from 'swiper/element';
+import { nuestroEquipo } from './matenimiento/nuestroEquipo';
 
 @Component({
   selector: 'app-home-nuestro-equipo',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   template: `
     <header>
       @defer (on viewport) {
-      <img class="equipo animate__animated animate__bounceInLeft" src="equipo-trabajo/equipo-trabajo.webp" alt="equipo de trabajo">
+      <swiper-container loop=true autoplay=true>
+        <swiper-slide *ngFor="let miembro of nuestroEquipo">
+          <img class="equipo animate__animated animate__bounceInLeft" [src]="miembro.src" [alt]="miembro.alt" />
+        </swiper-slide>
+      </swiper-container>
       }@placeholder {
         <div></div>
       }
-      @defer (on viewport){
+      @defer (on viewport) {
       <main class="animate__animated animate__bounceInRight">
         <div class="contenedor">
-        <span class="punto"></span>
-        <h1>Nuestro equipo de trabajo</h1></div>
+          <span class="punto"></span>
+          <h1>Nuestro equipo de trabajo</h1>
+        </div>
         <h2>con más de 10 capos que harán realidad tus locuras.</h2>
       </main>}
       @placeholder {
@@ -26,7 +32,17 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
       }
     </header>
   `,
-  styleUrl: './homeNuestroEquipo.component.css',
+  styleUrls: ['./homeNuestroEquipo.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeNuestroEquipoComponent { }
+export class HomeNuestroEquipoComponent {
+  nuestroEquipo = nuestroEquipo;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      register();
+    }
+  }
+}
---
+++ src/app/layouts/homeLayout/homeNuestroEquipo/matenimiento/nuestroEquipo.ts
@@ -0,0 +1,12 @@
+// src/app/nuestro-equipo.ts
+export const nuestroEquipo = [
+    { src: '/equipo-trabajo/Aslan.webp', alt: 'Aslan' },
+    { src: '/equipo-trabajo/equipo-trabajo.webp', alt: 'Equipo de Trabajo' },
+    { src: '/equipo-trabajo/Fatima.webp', alt: 'Fatima' },
+    { src: '/equipo-trabajo/Fiorella.webp', alt: 'Fiorella' },
+    { src: '/equipo-trabajo/Frank.webp', alt: 'Frank' },
+    { src: '/equipo-trabajo/Luis.webp', alt: 'Luis' },
+    { src: '/equipo-trabajo/Richy.webp', alt: 'Richy' },
+    { src: '/equipo-trabajo/Sergio.webp', alt: 'Sergio' },
+  ];
+  
